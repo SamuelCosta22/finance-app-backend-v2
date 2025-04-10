38629635cb6be31a8e6d7d23ed051bf8cb643751
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-04-10 13:47:50 -0300

    feat: add PostgresUpdateTransactionRepository

---
+++ src/repositories/postgres/transactions/update-transaction.repository.ts
@@ -0,0 +1,31 @@
+import { PostgresHelper } from '../../../db/postgres/helper.js';
+import { CreateTransactionsParams } from '../../../types/transactions/CreateTransactionParams.ts';
+
+export class PostgresUpdateTransactionRepository {
+  async execute(userId: string, input: CreateTransactionsParams) {
+    const updateFields: string[] = [];
+    const updateValues = [];
+
+    Object.keys(input).forEach((key) => {
+      const typedKey = key as keyof CreateTransactionsParams;
+      updateFields.push(`${key} = $${updateValues.length + 1}`);
+      updateValues.push(input[typedKey]);
+    });
+
+    updateValues.push(userId);
+
+    const updateQuery = `
+                UPDATE transactions
+                SET ${updateFields.join(', ')}
+                WHERE id = $${updateValues.length}
+                RETURNING *
+            `;
+
+    const updatedTransaction = await PostgresHelper.query(
+      updateQuery,
+      updateValues,
+    );
+
+    return updatedTransaction[0];
+  }
+}
