9e7145dd648c3a8884fe40dea02db5eacb772f33
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-04-14 14:56:22 -0300

    feat: ensure CreateTransaction and DeleteTransaction repository use Prisma

--- src/repositories/postgres/transactions/create-transaction.repository.ts
+++ src/repositories/postgres/transactions/create-transaction.repository.ts
@@ -1,20 +1,10 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js';
+import { prisma } from '../../../lib/prisma.ts';
 import { CreateTransactionsParams } from '../../../types/transactions/CreateTransactionParams.ts';
 
 export class PostgresCreateTransactionRepository {
   async execute(params: CreateTransactionsParams) {
-    const createdTransaction = await PostgresHelper.query(
-      'INSERT INTO transactions (id, user_id, name, date, amount, type) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;',
-      [
-        params.id,
-        params.user_id,
-        params.name,
-        params.date,
-        params.amount,
-        params.type,
-      ],
-    );
-
-    return createdTransaction[0];
+    return await prisma.transaction.create({
+      data: params,
+    });
   }
 }
--- src/repositories/postgres/transactions/delete-transaction.repository.ts
+++ src/repositories/postgres/transactions/delete-transaction.repository.ts
@@ -1,12 +1,16 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js';
+import { prisma } from '../../../lib/prisma.ts';
 
 export class PostgresDeleteTransactionRepository {
   async execute(transactionId: string) {
-    const transaction = await PostgresHelper.query(
-      'DELETE FROM transactions WHERE id = $1 RETURNING *;',
-      [transactionId],
-    );
-
-    return transaction[0];
+    try {
+      return await prisma.transaction.delete({
+        where: {
+          id: transactionId,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
   }
 }
