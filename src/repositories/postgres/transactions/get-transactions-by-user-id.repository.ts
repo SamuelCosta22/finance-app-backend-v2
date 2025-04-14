77630cb6dc1fc4eec842507d266172ad63e0dc9f
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-04-14 15:01:30 -0300

    feat: ensure UpdateTransaction and GetTransactionByUserId repository use Prisma

--- src/repositories/postgres/transactions/get-transactions-by-user-id.repository.ts
+++ src/repositories/postgres/transactions/get-transactions-by-user-id.repository.ts
@@ -1,12 +1,11 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js';
+import { prisma } from '../../../lib/prisma.ts';
 
 export class PostgresGetTransactionsByUserIdRepository {
   async execute(user_id: string) {
-    const transactions = await PostgresHelper.query(
-      'SELECT * FROM transactions WHERE user_id = $1',
-      [user_id],
-    );
-
-    return transactions;
+    return await prisma.transaction.findMany({
+      where: {
+        user_id,
+      },
+    });
   }
 }
--- src/repositories/postgres/transactions/update-transaction.repository.ts
+++ src/repositories/postgres/transactions/update-transaction.repository.ts
@@ -1,31 +1,13 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js';
+import { prisma } from '../../../lib/prisma.ts';
 import { CreateTransactionsParams } from '../../../types/transactions/CreateTransactionParams.ts';
 
 export class PostgresUpdateTransactionRepository {
   async execute(transactionId: string, input: CreateTransactionsParams) {
-    const updateFields: string[] = [];
-    const updateValues = [];
-
-    Object.keys(input).forEach((key) => {
-      const typedKey = key as keyof CreateTransactionsParams;
-      updateFields.push(`${key} = $${updateValues.length + 1}`);
-      updateValues.push(input[typedKey]);
+    return await prisma.transaction.update({
+      where: {
+        id: transactionId,
+      },
+      data: input,
     });
-
-    updateValues.push(transactionId);
-
-    const updateQuery = `
-                UPDATE transactions
-                SET ${updateFields.join(', ')}
-                WHERE id = $${updateValues.length}
-                RETURNING *
-            `;
-
-    const updatedTransaction = await PostgresHelper.query(
-      updateQuery,
-      updateValues,
-    );
-
-    return updatedTransaction[0];
   }
 }
