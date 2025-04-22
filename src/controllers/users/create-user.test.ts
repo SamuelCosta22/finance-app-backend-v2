9607e364323766f0c54aa5df0e3e7b1b4dfea2a3
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-04-22 16:03:15 -0300

    test: ensure 400 is returned if last_name is not provided

--- src/controllers/users/create-user.test.ts
+++ src/controllers/users/create-user.test.ts
@@ -48,4 +48,24 @@ describe('Create User Controller', () => {
     //assert
     expect(result.statusCode).toBe(400);
   });
+
+  it('should return 400 if last_name is not provided', async () => {
+    //arange
+    const createUserUseCase = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+
+    const httpRequest = {
+      body: {
+        first_name: 'Samuel',
+        email: 'samuelcosta@gmail.com',
+        password: '123456',
+      },
+    };
+
+    //act
+    const result = await createUserController.execute(httpRequest);
+
+    //assert
+    expect(result.statusCode).toBe(400);
+  });
 });
