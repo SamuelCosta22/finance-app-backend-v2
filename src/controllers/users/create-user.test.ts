deafb18132d3a92af657b25f37bc868a6992a267
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-04-22 16:21:15 -0300

    test: ensure 400 is returned if lemail is not provided

--- src/controllers/users/create-user.test.ts
+++ src/controllers/users/create-user.test.ts
@@ -68,4 +68,24 @@ describe('Create User Controller', () => {
     //assert
     expect(result.statusCode).toBe(400);
   });
+
+  it('should return 400 if email is not provided', async () => {
+    //arange
+    const createUserUseCase = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+
+    const httpRequest = {
+      body: {
+        first_name: 'Samuel',
+        last_name: 'Costa',
+        password: '123456',
+      },
+    };
+
+    //act
+    const result = await createUserController.execute(httpRequest);
+
+    //assert
+    expect(result.statusCode).toBe(400);
+  });
 });
