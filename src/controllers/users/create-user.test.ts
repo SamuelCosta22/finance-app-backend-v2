8c5f4a49ed42be77f8b3f7fc86da1113c1ae2df1
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-04-22 16:23:50 -0300

    test: ensure 400 is returned if password is not provided

--- src/controllers/users/create-user.test.ts
+++ src/controllers/users/create-user.test.ts
@@ -109,4 +109,24 @@ describe('Create User Controller', () => {
     //assert
     expect(result.statusCode).toBe(400);
   });
+
+  it('should return 400 if password is not provided', async () => {
+    //arange
+    const createUserUseCase = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+
+    const httpRequest = {
+      body: {
+        first_name: 'Samuel',
+        last_name: 'Costa',
+        email: 'samuelcosta@gmail.com',
+      },
+    };
+
+    //act
+    const result = await createUserController.execute(httpRequest);
+
+    //assert
+    expect(result.statusCode).toBe(400);
+  });
 });
