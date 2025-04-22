0562b0be1e3e6305f941934f4333d56fcbdd969a
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-04-22 16:22:32 -0300

    test: ensure 400 is returned if lemail is not valid

--- src/controllers/users/create-user.test.ts
+++ src/controllers/users/create-user.test.ts
@@ -88,4 +88,25 @@ describe('Create User Controller', () => {
     //assert
     expect(result.statusCode).toBe(400);
   });
+
+  it('should return 400 if email is not valid', async () => {
+    //arange
+    const createUserUseCase = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+
+    const httpRequest = {
+      body: {
+        first_name: 'Samuel',
+        last_name: 'Costa',
+        email: 'samuelcosta',
+        password: '123456',
+      },
+    };
+
+    //act
+    const result = await createUserController.execute(httpRequest);
+
+    //assert
+    expect(result.statusCode).toBe(400);
+  });
 });
