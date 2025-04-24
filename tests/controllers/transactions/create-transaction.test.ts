400a5adca4d2f55ee60e6a3eb607d78cf387f6eb
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-04-24 15:27:05 -0300

    test: ensure CreateTransactionController returns 400 when missing type

--- tests/controllers/transactions/create-transaction.test.ts
+++ tests/controllers/transactions/create-transaction.test.ts
@@ -85,4 +85,20 @@ describe('Create Transaction Controller', () => {
     //assert
     expect(result.statusCode).toBe(400);
   });
+
+  it('should return 400 when missing type', async () => {
+    //arrange
+    const { sut } = makeSut();
+
+    //act
+    const result = await sut.execute({
+      body: {
+        ...httpRequest.body,
+        type: undefined,
+      } as any,
+    });
+
+    //assert
+    expect(result.statusCode).toBe(400);
+  });
 });
