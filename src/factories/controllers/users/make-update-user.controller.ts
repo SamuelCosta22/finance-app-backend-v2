0a182a887a69527412543cc2e97f4719671780a2
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-04-28 10:49:09 -0300

    feat: ensure UpdateUserUseCase uses PasswordHasherAdapter

--- src/factories/controllers/users/make-update-user.controller.ts
+++ src/factories/controllers/users/make-update-user.controller.ts
@@ -1,10 +1,15 @@
+import { PasswordHasherAdapter } from '../../../adapters/password-hasher.ts';
 import { UpdateUserController } from '../../../controllers/users/update-user.controller.ts';
 import { PostgresUpdateUserRepository } from '../../../repositories/postgres/users/update-user.repository.ts';
 import { UpdateUserUseCase } from '../../../usecases/users/update-user.usecase.ts';
 
 export const makeUpdateUserController = () => {
   const updateUserRepository = new PostgresUpdateUserRepository();
-  const updateUserUseCase = new UpdateUserUseCase(updateUserRepository);
+  const passwordHasherAdapter = new PasswordHasherAdapter();
+  const updateUserUseCase = new UpdateUserUseCase(
+    updateUserRepository,
+    passwordHasherAdapter,
+  );
   const updateUserController = new UpdateUserController(updateUserUseCase);
 
   return updateUserController;
--- src/usecases/users/update-user.usecase.ts
+++ src/usecases/users/update-user.usecase.ts
@@ -1,12 +1,16 @@
-import bcrypt from 'bcrypt';
 import { EmailAlreadyInUseError } from '../../errors/user.ts';
 import { PostgresGetUserByEmailRepository } from '../../repositories/postgres/users/get-user-by-email.repository.ts';
 import { CreateUserParams } from '../../types/users/CreateUserParams.ts';
 import { IUpdateUserRepository } from '../../types/repositories/users.repository.ts';
+import { IHashGeneratorAdapter } from '../../adapters/password-hasher.ts';
 
 export class UpdateUserUseCase {
-  constructor(private updateUserRepository: IUpdateUserRepository) {
+  constructor(
+    private updateUserRepository: IUpdateUserRepository,
+    private passwordHasherAdapter: IHashGeneratorAdapter,
+  ) {
     this.updateUserRepository = updateUserRepository;
+    this.passwordHasherAdapter = passwordHasherAdapter;
   }
 
   async execute(userId: string, input: CreateUserParams) {
@@ -24,7 +28,9 @@ export class UpdateUserUseCase {
     const user = { ...input };
 
     if (input.password) {
-      const hashedPassword = await bcrypt.hash(input.password, 10);
+      const hashedPassword = await this.passwordHasherAdapter.execute(
+        input.password,
+      );
       user.password = hashedPassword;
     }
 
