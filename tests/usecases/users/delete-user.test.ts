94fbae667c6c0e3526e2094620bf73f89c2237f3
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-04-29 09:08:34 -0300

    test: ensure DeleteUserUseCase calls DeleteUserRepository with correct params

--- tests/usecases/users/delete-user.test.ts
+++ tests/usecases/users/delete-user.test.ts
@@ -34,4 +34,17 @@ describe('Delete User Use Case', () => {
     //assert
     expect(deletedUser).toEqual(user);
   });
+
+  it('should call DeleteUserRepository with correct params', async () => {
+    //arrange
+    const { deleteUserRepository, sut } = makeSut();
+    const executeSpy = jest.spyOn(deleteUserRepository, 'execute');
+    const userId = faker.string.uuid();
+
+    //act
+    await sut.execute(userId);
+
+    //assert
+    expect(executeSpy).toHaveBeenCalledWith(userId);
+  });
 });
