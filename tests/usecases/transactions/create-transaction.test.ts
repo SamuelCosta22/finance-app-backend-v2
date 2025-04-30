ed6484aa57fad2667cc668a6bddf1af057a7572a
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-04-30 16:00:18 -0300

    test: ensure CreateTransactionUseCase throws if IdGeneratorAdapter throws

--- tests/usecases/transactions/create-transaction.test.ts
+++ tests/usecases/transactions/create-transaction.test.ts
@@ -151,4 +151,18 @@ describe('Create Transaction Use Case', () => {
     //assert
     await expect(promise).rejects.toThrow();
   });
+
+  it('should throw if IdGeneratorAdapter throws', async () => {
+    //arrange
+    const { idGeneratorAdapter, sut } = makeSut();
+    jest.spyOn(idGeneratorAdapter, 'execute').mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    //act
+    const promise = sut.execute(createTransactionParams);
+
+    //assert
+    await expect(promise).rejects.toThrow();
+  });
 });
