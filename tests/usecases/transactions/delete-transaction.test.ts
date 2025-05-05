58eb131f22c7a5a6da2786dc80e7886aeeef572f
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-05-05 08:32:32 -0300

    test: ensure DeleteTransactionUseCase deletes a transaction successfully

---
+++ tests/usecases/transactions/delete-transaction.test.ts
@@ -0,0 +1,44 @@
+import { faker } from '@faker-js/faker';
+import { TransactionEnum } from '../../../generated/prisma/client.js';
+import { DeleteTransactionUseCase } from '../../../src/usecases/transactions/delete-transaction.usecase.ts';
+
+const transaction = {
+  id: faker.string.uuid(),
+  user_id: faker.string.uuid(),
+  name: faker.person.fullName(),
+  date: faker.date.anytime(),
+  amount: Number(faker.finance.amount()),
+  type: TransactionEnum.EARNING,
+};
+
+class DeleteTransactionRepositoryStub {
+  async execute(transactionId: string) {
+    return {
+      ...transaction,
+      id: transactionId,
+    };
+  }
+}
+
+describe('Create Transaction Use Case', () => {
+  const makeSut = () => {
+    const deleteTransactionRepository = new DeleteTransactionRepositoryStub();
+    const sut = new DeleteTransactionUseCase(deleteTransactionRepository);
+
+    return {
+      deleteTransactionRepository,
+      sut,
+    };
+  };
+
+  it('should delete transaction successfully', async () => {
+    //arrange
+    const { sut } = makeSut();
+
+    //act
+    const result = await sut.execute(transaction.id);
+
+    //assert
+    expect(result).toEqual({ ...transaction, id: transaction.id });
+  });
+});
