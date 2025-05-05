36fbafa016f91eacba262913c11addea2f7c377c
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-05-05 10:05:32 -0300

    test: ensure UpdateTransactionUseCase updates transaction successfully

---
+++ tests/usecases/transactions/update-transaction.test.ts
@@ -0,0 +1,40 @@
+import { faker } from '@faker-js/faker';
+import { UpdateTransactionUseCase } from '../../../src/usecases/transactions/update-transaction.usecase.ts';
+import { TransactionEnum } from '../../../generated/prisma/client.js';
+
+const transaction = {
+  id: faker.string.uuid(),
+  user_id: faker.string.uuid(),
+  name: faker.person.fullName(),
+  date: faker.date.anytime(),
+  amount: Number(faker.finance.amount()),
+  type: TransactionEnum.EARNING,
+};
+
+class UpdateTransactionRepositoryStub {
+  async execute(transactionId: string) {
+    return { ...transaction, id: transactionId };
+  }
+}
+
+describe('Update Transaction Use Case', () => {
+  const makeSut = () => {
+    const updateTransactionRepository = new UpdateTransactionRepositoryStub();
+    const sut = new UpdateTransactionUseCase(updateTransactionRepository);
+
+    return { updateTransactionRepository, sut };
+  };
+
+  it('should update transaction successfully', async () => {
+    //arrange
+    const { sut } = makeSut();
+
+    //act
+    const result = await sut.execute(transaction.id, {
+      amount: Number(faker.finance.amount()),
+    });
+
+    //assert
+    expect(result).toEqual(transaction);
+  });
+});
