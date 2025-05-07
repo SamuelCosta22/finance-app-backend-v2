a42555824a7f27edf94941229c93076ca07f8363
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-05-07 14:50:43 -0300

    test: ensure GET /api/users/:userId/balance returns 200 when user balance is found (e2e)

--- tests/routes/user.e2e.test.ts
+++ tests/routes/user.e2e.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { user } from '../fixtures/user.ts';
 import { app } from '../../src/app.ts';
 import { faker } from '@faker-js/faker';
+import { TransactionEnum } from '../../src/types/transactions/CreateTransactionParams.ts';
 
 describe('User Routes E2E Tests', () => {
   it('POST /api/users should return 201 when user is created', async () => {
@@ -58,5 +59,48 @@ describe('User Routes E2E Tests', () => {
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual(createdUser);
+  });
+
+  it('GET /api/users/:userId/balance should return 200 when user balance is found', async () => {
+    const { body } = await request(app)
+      .post('/api/users')
+      .send({ ...user, id: undefined });
+    const createdUser = body.createdUser;
+
+    await request(app).post('/api/transactions').send({
+      user_id: createdUser.id,
+      name: faker.commerce.productName(),
+      date: faker.date.anytime(),
+      type: TransactionEnum.EARNING,
+      amount: 10000,
+    });
+
+    await request(app).post('/api/transactions').send({
+      user_id: createdUser.id,
+      name: faker.commerce.productName(),
+      date: faker.date.anytime(),
+      type: TransactionEnum.EXPENSE,
+      amount: 2000,
+    });
+
+    await request(app).post('/api/transactions').send({
+      user_id: createdUser.id,
+      name: faker.commerce.productName(),
+      date: faker.date.anytime(),
+      type: TransactionEnum.INVESTMENT,
+      amount: 1000,
+    });
+
+    const response = await request(app).get(
+      `/api/users/${createdUser.id}/balance`,
+    );
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      earnings: 10000,
+      expenses: 2000,
+      investments: 1000,
+      balance: 7000,
+    });
   });
 });
