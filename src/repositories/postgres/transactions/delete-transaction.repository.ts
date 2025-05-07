523fac58d6d9fdd6e504de9b6b884a32ea6a2ca8
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-05-07 10:30:49 -0300

    feat: ensure PostgresDeleteTransactionRepository throws if Prisma throws

--- src/repositories/postgres/transactions/delete-transaction.repository.ts
+++ src/repositories/postgres/transactions/delete-transaction.repository.ts
@@ -1,3 +1,5 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { TransactionNotFoundError } from '../../../errors/transaction.ts';
 import { prisma } from '../../../lib/prisma.ts';
 
 export class PostgresDeleteTransactionRepository {
@@ -9,8 +11,14 @@ export class PostgresDeleteTransactionRepository {
         },
       });
     } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        //P2025 - "An operation failed because it depends on one or more records that were required but could not be found."
+        if (error.code === 'P2025') {
+          throw new TransactionNotFoundError(transactionId);
+        }
+      }
       console.error(error);
-      return null;
+      throw error;
     }
   }
 }
