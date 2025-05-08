e10e2a3d88fe2102c8fbe6a193a4367e2bbad969
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-05-08 13:46:54 -0300

    chore: add initial swagger setup

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import 'dotenv/config.js';
 import express from 'express';
 import { transactionRouter } from './routes/transaction.ts';
 import { userRouter } from './routes/user.ts';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import path from 'path';
 
 export const app = express();
 
@@ -11,3 +14,12 @@ app.use(express.json());
 
 app.use('/api/users', userRouter);
 app.use('/api/transactions', transactionRouter);
+
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(
+    path.join(import.meta.dirname, '../docs/swagger.json'),
+    'utf8',
+  ),
+);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
