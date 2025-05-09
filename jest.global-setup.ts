8b485fb91a882c6b0a345122ab759dfcc8f77f70
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-05-09 12:42:20 -0300

    chore: add configuration to run test db before execution

--- jest.global-setup.ts
+++ jest.global-setup.ts
@@ -1,6 +1,13 @@
 import { execSync } from 'child_process';
+import dotenv from 'dotenv';
+import path from 'path';
 
 export default async function () {
   console.log('🔄 Sincronizando banco no NeonDB...');
+
+  // Carrega as variáveis do .env.test
+  dotenv.config({ path: path.resolve(process.cwd(), '.env.test') });
+
+  // Agora que DATABASE_URL está no process.env, o Prisma pode usar
   execSync('npx prisma db push', { stdio: 'inherit' });
 }
