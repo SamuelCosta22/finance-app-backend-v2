a0fda54ccaad2266b28054f9b6126b354251eab8
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-05-12 13:02:23 -0300

    chore: update eslint version

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+
+export default await tseslint.config({
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+  ],
+  files: ['**/*.ts', '**/*.tsx'],
+  languageOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    globals: {
+      ...globals.node,
+      ...globals.jest,
+    },
+    parserOptions: {
+      project: ['./tsconfig.json'],
+    },
+  },
+  plugins: {
+    prettier,
+  },
+  rules: {
+    'prettier/prettier': ['error', { semi: true }],
+    semi: ['error', 'always'],
+    '@typescript-eslint/no-unused-vars': 'error',
+
+    //TO-DO: create interfaces and types to all functions and methods
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+  },
+});
