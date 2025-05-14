96b504121a8bd1e67f22e3ddd7d0424bb46b5fc9
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-05-14 14:04:15 -0300

    test: ensure user factories are tested correctly

---
+++ tests/factories/controllers/users/make-login-user.test.ts
@@ -0,0 +1,8 @@
+import { LoginUserController } from '../../../../src/controllers/users/login-user.controller.ts';
+import { makeLoginUserController } from '../../../../src/factories/controllers/users/make-login-user.controller.ts';
+
+describe('Make Update User Factory', () => {
+  it('should return a valid LoginUserControllerInstance', () => {
+    expect(makeLoginUserController()).toBeInstanceOf(LoginUserController);
+  });
+});
