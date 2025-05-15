1a6dea71f4f0c44fe09a2773b298e0f738b5d941
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-05-15 12:06:31 -0300

    fix: ensure timeout error does not occur

--- jest.setup-after-env.ts
+++ jest.setup-after-env.ts
@@ -1,6 +1,13 @@
 import { prisma } from './src/lib/prisma.ts';
+import { jest } from '@jest/globals';
+
+jest.setTimeout(30000);
 
 beforeEach(async () => {
   await prisma.transaction.deleteMany({});
   await prisma.user.deleteMany({});
+});
+
+afterAll(async () => {
+  await prisma.$disconnect();
 });
