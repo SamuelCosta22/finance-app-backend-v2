036e8f81bc021ba5267f9805605667237082a2dd
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-05-16 14:00:01 -0300

    feat: ensure GetUserBalanceRepository receives 'from' and 'to' params

--- src/repositories/postgres/users/get-user-balance.repository.ts
+++ src/repositories/postgres/users/get-user-balance.repository.ts
@@ -2,13 +2,21 @@ import { prisma } from '../../../lib/prisma.ts';
 import { TransactionEnum } from '../../../types/transactions/CreateTransactionParams.ts';
 
 export class PostgresGetUserBalanceRepository {
-  async execute(userId: string) {
+  async execute(userId: string, from: Date, to: Date) {
+    const dateFilter = {
+      date: {
+        gte: new Date(from),
+        lte: new Date(to),
+      },
+    };
+
     const {
       _sum: { amount: totalExpenses },
     } = await prisma.transaction.aggregate({
       where: {
         user_id: userId,
         type: TransactionEnum.EXPENSE,
+        ...dateFilter,
       },
       _sum: {
         amount: true,
@@ -21,6 +29,7 @@ export class PostgresGetUserBalanceRepository {
       where: {
         user_id: userId,
         type: TransactionEnum.EARNING,
+        ...dateFilter,
       },
       _sum: {
         amount: true,
@@ -33,6 +42,7 @@ export class PostgresGetUserBalanceRepository {
       where: {
         user_id: userId,
         type: TransactionEnum.INVESTMENT,
+        ...dateFilter,
       },
       _sum: {
         amount: true,
